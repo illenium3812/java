8fbeb796c69a442d55a8895b98a09f30abac2a49
Mansoor Khan <41062288+illenium3812@users.noreply.github.com>
2020-02-22 14:02:44 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,39 +1,86 @@
-package OOPlab;
-
-
-import OOPlab.BankAccount;
-
-
-
-
+package com.company;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
 
-   
     public static void main(String[] args) {
-        
-        
-        BankAccount.bonus(10); // changing this will change add/remove bonus amount from all accounts 
-        BankAccount []accounts = new BankAccount[5]; // creating array of 5 elements of type BankAccount class
-        accounts[0] = new BankAccount("Mansoor",58,"152-E",10000);
-        accounts[1] = new BankAccount("Faizan",037,"152-E",5000);
-        accounts[2] = new BankAccount("Junaid",041,"152-E",1000);
-        accounts[3] = new BankAccount("Ammad",061,"152-E",500);
-        accounts[4] = new BankAccount("Khizar",115,"152-E",100);
-        
-        
-        
-        
-        
-        for (int i = 0 ; i < accounts.length ; i++ ) //loop for displaying balanceInfo for all accounts in array
+	int x = 0;
+	String binary;
+	String num2;
+	Scanner sc = new Scanner(System.in);
+	while (x != 1) {
+	    binary="";
+
+        System.out.println("Press 1 for Decimal to Binary Conversion");
+        System.out.println("Press 2 for Binary to Decimal Conversion");
+        int choice = sc.nextInt();
+        if (choice == 1)
         {
-            accounts[i].balanceInfo();
-            System.out.print("\n\n");
+            System.out.println("Enter Decimal Number: ");
+            int num = sc.nextInt();
+            if (num == 0){
+                System.out.println("00000000");
+            }
+            while(num!=0 && num>0){
+                int remainder = num%2;
+                num = num/2;
+                if (remainder == 1)
+                {
+                    binary = 1 + binary;
+                }
+                else
+                {
+                    binary = 0+binary;
+                }
+            }
+            System.out.println("Your Answer is  " + binary);
+
+
         }
-       
-        
-      
-        
-      }
-    
+        else if (choice == 2)
+        {
+            System.out.println("Enter Binary Number: ");
+             num2 = sc.next();
+             int lenght = num2.length();
+             char holder;
+             int power = 0;
+             int  num = 0;
+             for (int i = lenght-1 ;i != -1;i-- )
+             {
+                 holder =num2.charAt(i);
+
+                 if (holder == '0'){
+                     power++;
+                 }
+                 if (holder == '1')
+                 {
+                     num = (int)(num + Math.pow(2,power));
+                     power++;
+                 }
+             }
+             System.out.println("Your Answer : " + num);
+
+        }
+        else{
+            System.out.println("Invalid selection! Please press 1 or 2");
+        }
+
+
+
+
+
+
+        System.out.println("Do you wanna Convert Next? Press 1 for Yes Press any key to exit");
+        int exit = (int)(sc.nextInt());
+        if (exit == 1){}
+        else{
+            x = 1;
+        }
+
+
+
+    }
+
+    }
 }
